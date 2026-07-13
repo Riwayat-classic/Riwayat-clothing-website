d74fb6375174a4e47a56257e7ff168e821287252
Riwayat-classic <riwayat.businesses@gmail.com>
2026-07-13 15:46:43 +0500

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -125,7 +125,7 @@ export function Navbar() {
         />
         {/* Panel */}
         <div
-          className={`absolute right-0 top-0 flex h-full w-72 max-w-[80%] flex-col bg-maroon-dark p-6 shadow-2xl transition-transform duration-300 ${
+          className={`fixed right-0 top-0 z-[9999] flex h-screen w-72 max-w-[80%] flex-col overflow-y-auto bg-maroon-dark p-6 shadow-2xl transition-transform duration-300 ${
             open ? "translate-x-0" : "translate-x-full"
           }`}
         >
