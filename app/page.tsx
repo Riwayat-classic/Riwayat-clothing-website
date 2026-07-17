7895fb768962e8a7dba1975a6f83acc8a6cec4ca
Riwayat-classic <riwayat.businesses@gmail.com>
2026-07-17 10:33:17 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default async function Page() {
 
       <FeaturedProducts products={products} />
 
-      <ServicesOverview services={content.services} />
+      {/* <ServicesOverview services={content.services} /> */}
 
       <PromoBanners promos={content.promos} />
 
