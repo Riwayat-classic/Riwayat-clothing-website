4e9b0f2e990c453ac5603316e796ff7d66d37a58
Riwayat-classic <riwayat.businesses@gmail.com>
2026-09-17 22:07:42 +0500

    Update contact-info.tsx

--- components/contact/contact-info.tsx
+++ components/contact/contact-info.tsx
@@ -16,7 +16,7 @@ export async function ContactInfo() {
     icon: MapPin,
     title: "Visit Our Store",
     value: contact.address,
-    link: "https://www.google.com/maps/place/Riwayat+Designers/@24.8661813,67.0583741,20z",
+    link: "https://www.google.com/maps/place/Riwayat+Designers/@24.8661813,67.0583741,10z",
   },
   {
     icon: Phone,
